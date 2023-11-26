a094bbf1532e993fac1e057c33bfa88221be2669
Aiden <42153616+aidenx11@users.noreply.github.com>
2023-11-26 16:19:55 -0500

    Add classes, which have method shells set up

---
+++ Polynomial.java
@@ -0,0 +1,33 @@
+public class Polynomial {
+
+    public Polynomial() {
+
+    }
+
+    public void addCoefficient(double coefficient) {
+
+    }
+
+    public double[] getPolynomial() {
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+
+        return null;
+    }
+
+    public double getValue(double x) {
+
+        return 0;
+    }
+
+    public int findRoot(double lower, double upper) {
+
+        return 0;
+    }
+
+
+}
---
+++ PolynomialUI.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PolynomialUI {
+
+    public static void main(String[] args) {
+        Polynomial polynomial = new Polynomial();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type in the polynomials in increasing powers: ");
+    }
+
+    public void runPolynomial() {
+
+    }
+
+    public void setPolynomial() {
+
+    }
+
+    public void printPolynomial() {
+
+    }
+
+    public void setRange() {
+
+    }
+
+    public void printTable() {
+
+    }
+
+}
